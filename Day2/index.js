4a6a0e3ee34dacc5ce125aef01de5440c90a49ef
Priyam Patra <priyampatra63@gmail.com>
2025-12-08 20:07:15 +0530

    Update API key placeholder in index.js

--- Day2/index.js
+++ Day2/index.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: "AIzaSyCLvIjJBrGkX1EnQIZN__5PPc3SxdFr4U8",
+  apiKey: "INSERT YOUR API KEY HERE",
 });
 
 async function main() {
